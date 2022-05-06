8b1f05f1f6094c04dd510ae7b3d94e9ab207b26e
Szybet <53944559+Szybet@users.noreply.github.com>
2022-05-06 09:55:25 +0200

    Update kobo-qt-in-debian-container.sh

--- apps-on-kobo/kobo-qt-in-debian-container.sh
+++ apps-on-kobo/kobo-qt-in-debian-container.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+mkdir qt-kobo
+cd qt-kobo
+sudo apt-get install git
+git clone https://github.com/koreader/koxtoolchain
+cd koxtoolchain
+
+
+
+sudo apt-get install build-essential autoconf automake bison flex gawk libtool libtool-bin libncurses-dev curl file git gperf help2man texinfo unzip wget
+wget https://www.openssl.org/source/openssl-1.1.1n.tar.gz
+tar -xf openssl-1.1.1n.tar.gz
+cd openssl-1.1.1n
