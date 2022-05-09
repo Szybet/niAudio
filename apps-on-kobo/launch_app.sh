294668866b74c0fff2c4f3a15e53048405e5be1c
Szybet <53944559+Szybet@users.noreply.github.com>
2022-05-09 22:34:04 +0200

    Update launch_app.sh

--- apps-on-kobo/launch_app.sh
+++ apps-on-kobo/launch_app.sh
@@ -12,5 +12,8 @@ ash -c "killall -9 inkbox.sh inkbox inkbox-bin exec; sleep 1; killall -9 inkbox.
 fbink -c
 chroot /kobo /bin/ash -c "env LD_LIBRARY_PATH=/mnt/onboard/.adds/qt-linux-5.15.2-kobo/lib QT_QPA_PLATFORM=kobo /tmp/exec"
 # for inkbox for example:
+# First with this setup umount those libraries:
+# umount /mnt/onboard/.adds/qt-linux-5.15.2-kobo/plugins/platforms/libkobo.so
+# umount /mnt/onboard/.adds/qt-linux-5.15.2-kobo
 # chroot /kobo /bin/ash -c "env LD_LIBRARY_PATH="/qt-linux-5.15.2-kobo/lib:lib" QTPATH="/qt-linux-5.15.2-kobo" QT_QPA_PLATFORM=kobo ADDSPATH="/mnt/onboard/.adds/" PATH="${PATH}:/mnt/onboard/.adds/Python-3.9.2/" DEBUG=true QT_FONT_DPI=$(cat /kobo/mnt/onboard/.adds/inkbox/.config/09-dpi/config) LD_PRELOAD=/lib/invert_screen.so /tmp/inkbox"
 fbink -c
