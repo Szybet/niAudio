23b8d4f7d68131275c72f2efd88b7bbf6299a317
Szybet <53944559+Szybet@users.noreply.github.com>
2022-05-09 21:33:10 +0200

    Update launch_app.sh

--- apps-on-kobo/launch_app.sh
+++ apps-on-kobo/launch_app.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This file is intended to be launched by qt creator as a custom executable, for inkbox os
+# LD_LIBRARY_PATH can be your qt, or used by inkbox
 
 # Before launching this script its needed to add those custom steps:
 # ifsctl mnt rootfs rw
@@ -10,4 +11,6 @@ cd /
 ash -c "killall -9 inkbox.sh inkbox inkbox-bin exec; sleep 1; killall -9 inkbox.sh inkbox inkbox-bin exec" || EXIT_CODE=0
 fbink -c
 chroot /kobo /bin/ash -c "env LD_LIBRARY_PATH=/mnt/onboard/.adds/qt-linux-5.15.2-kobo/lib QT_QPA_PLATFORM=kobo /tmp/exec"
+# If needed, for inkbox for example:
+# chroot /kobo /bin/ash -c "env LD_LIBRARY_PATH="/qt-linux-5.15.2-kobo/lib:lib" QTPATH="/qt-linux-5.15.2-kobo" QT_QPA_PLATFORM=kobo ADDSPATH="/" /tmp/inkbox"
 fbink -c
