fbd0537bd132c850caf67653b331a7947e0ca2ff
Szybet <53944559+Szybet@users.noreply.github.com>
2022-06-19 19:39:11 +0200

    Update launch_app.sh

--- apps-on-kobo/launch_app.sh
+++ apps-on-kobo/launch_app.sh
@@ -19,3 +19,19 @@ chroot /kobo /bin/ash -c "env LD_LIBRARY_PATH=/mnt/onboard/.adds/qt-linux-5.15.2
 # umount /kobo/mnt/onboard/.adds/qt-linux-5.15.2-kobo
 # chroot /kobo /bin/ash -c "env LD_LIBRARY_PATH="/qt-linux-5.15.2-kobo/lib:lib" QTPATH="/qt-linux-5.15.2-kobo" QT_QPA_PLATFORM=kobo ADDSPATH="/mnt/onboard/.adds/" PATH="${PATH}:/mnt/onboard/.adds/Python-3.9.2/" DEBUG=true QT_FONT_DPI=$(cat /kobo/mnt/onboard/.adds/inkbox/.config/09-dpi/config) LD_PRELOAD=/lib/invert_screen.so /tmp/inkbox"
 fbink -c
+
+# This approach for qt creator is faster, just launch the script:
+##!/bin/bash
+
+#mv inkbox exec
+
+#servername="root@10.42.0.28"
+#passwd="root"
+
+#sshpass -p $passwd ssh $servername "bash -c \"ifsctl mnt rootfs rw\""
+#sshpass -p $passwd ssh $servername "bash -c \"rm /kobo/tmp/exec\""
+#sshpass -p $passwd scp exec $servername:/kobo/tmp/
+
+#sshpass -p $passwd ssh $servername "bash -c \"sync\""
+
+#sshpass -p $passwd ssh $servername "bash -c \"/kobo/launch_app.sh\""
