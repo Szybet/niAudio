78b00794c7048c6eb974ac3196d13554bbe5663c
Szybet <53944559+Szybet@users.noreply.github.com>
2022-08-27 18:22:48 +0200

    Update launch_app.sh

--- apps-on-kobo/launch_app.sh
+++ apps-on-kobo/launch_app.sh
@@ -20,6 +20,11 @@ chroot /kobo /bin/ash -c "env LD_LIBRARY_PATH=/mnt/onboard/.adds/qt-linux-5.15.2
 # chroot /kobo /bin/ash -c "env LD_LIBRARY_PATH="/qt-linux-5.15.2-kobo/lib:lib" QTPATH="/qt-linux-5.15.2-kobo" QT_QPA_PLATFORM=kobo ADDSPATH="/mnt/onboard/.adds/" PATH="${PATH}:/mnt/onboard/.adds/Python-3.9.2/" DEBUG=true QT_FONT_DPI=$(cat /kobo/mnt/onboard/.adds/inkbox/.config/09-dpi/config) LD_PRELOAD=/lib/invert_screen.so /tmp/inkbox"
 fbink -c
 
+# the proper way for inkbox: bind mount it to the place it should
+#umount -f /kobo/mnt/onboard/.adds/inkbox/inkbox-bin
+#mount -o ro,bind /kobo/tmp/exec /kobo/mnt/onboard/.adds/inkbox/inkbox-bin
+#/usr/bin/inkbox.sh; read;
+
 # This approach for qt creator is faster, just launch the script:
 ##!/bin/bash
 
